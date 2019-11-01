545ff5e8c7641b2032ab71ad8467d3bc3da2ddff
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 14:02:16 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :artists, only: [:create, :update, :show, :new, :edit]
+  resources :genres, only: [:create, :update, :show, :new, :edit]
+  resources :songs, only: [:create, :update, :show, :edit, :new, :index]
 end
